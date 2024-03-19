23216af8a03e12ce115ef4b411cea6f21dbf6f9d
Oscar Cadena <oscar7913@gmail.com>
2024-03-19 17:31:53 -0400

    Challenge 3

---
+++ JS-challenge-3/Challenge3.js
@@ -0,0 +1,38 @@
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = 'HttpError';
+    this.response = response;
+  }
+}
+
+async function loadJson(url) {
+    try {
+        const response = await fetch(url)
+        if (response.status === 200){
+            return response.json();
+        }
+        else{
+            throw new HttpError(response);
+        }
+    } catch (error) {
+        throw new HttpError(error);
+    }
+}
+
+async function demoGithubUserAsync(){
+    try {
+        let name = prompt('Enter a name?', 'vanimar');
+        const loadJsonAsync =  await loadJson(`https://api.github.com/users/${name}`);
+        alert(`Full name: ${loadJsonAsync.name}.`);
+        return user
+    } catch (error) {
+        if (error instanceof HttpError && err.response.status == 404) {
+            alert('No such user, please reenter.');
+            return demoGithubUser();
+          } else {
+            throw err;
+          }
+    }
+}
+demoGithubUserAsync();oti
